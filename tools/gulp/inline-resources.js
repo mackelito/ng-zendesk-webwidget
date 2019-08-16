7391513b33a97277f6276e8e747514d4c09a7eac
Markus Thiel <markus@thiels.se>
2019-08-16 12:13:38 +0200

    updated to sass instead of node-sass

--- tools/gulp/inline-resources.js
+++ tools/gulp/inline-resources.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
-const sass = require('node-sass');
+const sass = require('sass');
 const tildeImporter = require('node-sass-tilde-importer');
 
 /**
